14ece3c35f9315fd9f811e44908d73da6aaee2f2
neleph <luc.moutanin@gmail.com>
2024-04-08 02:18:52 +0400

    menu

--- Login/menu.php
+++ Login/menu.php
@@ -1,5 +1,13 @@
 <?php 
+ 
+
 function nombre($a) {
+
+    require('client.php');
+    $prenom = $rep['prenom'];
+    $nom = $rep['nom'];
+    
+
     $un = '';
     $deux = '';
     $trois = '';
@@ -18,21 +26,22 @@ function nombre($a) {
         $quatre = 'active';
     }
 
-    echo '
+ 
 
-<nav>
-    <ul>
-        <li><a class="' . $un . '" href="page.php">Home</a></li>
-        <li><a class="' . $deux . '" href="page1.php">News</a></li>
-        <li><a class="' . $trois . '" href="page2.php">Contact</a></li>
-        <li><a class="' . $quatre . '" href="page3.php">About</a></li>
-    </ul>
+ 
 
-</nav>
-    ';
-}
+echo '
+<ul>
+    <li><strong><p>Bonjour</p><p>'. $nom."  ". $prenom .'</p> </strong>
+     <button class="button-66" role="button"><strong> Déconnexion</strong</button></li>
+    <li><a class="' . $un . '" href="page.php">Home</a></li>
+    <li><a class="' . $deux . '" href="page1.php">News</a></li>
+    <li><a class="' . $trois . '" href="page2.php">Contact</a></li>
+    <li><a class="' . $quatre . '" href="page3.php">About</a></li>
+</ul>
+';
+} 
  
 ?>
-
 
  
