8621fe950d21661aa787f1034def5e7587ab559a
Luc Moutanin <luc.moutanin@gmail.com>
2024-04-16 00:53:30 +0400

    modification de la class voiture

--- Login/class/Voiture.php
+++ Login/class/Voiture.php
@@ -11,13 +11,14 @@ class Voiture
 
 public function __construct($marque,$immatriculation,$modele,$kilometrage,$annee,$id_client)
  {
-    $marque=$this->filtre($marque);
-    $immatriculation=$this->filtre($immatriculation);
-    $modele=$this->filtre($modele);
-    $kilometrage=$this->filtre($kilometrage);
-    $annee=$this->filtre($annee);
-    $id_client=$this->filtre($id_client);
-    
+    $marque = $this->filtre($marque);
+    $immatriculation = $this->filtre($immatriculation);
+    $modele = $this->filtre($modele);
+    $kilometrage = $this->filtre($kilometrage);
+    $annee = $this->filtre($annee);
+    $id_client = $this->filtre($id_client);
+
+
     $this->marque=$marque;
     $this->immatriculation=$immatriculation;
     $this->modele=$modele;
@@ -27,11 +28,13 @@ public function __construct($marque,$immatriculation,$modele,$kilometrage,$annee
 
 }
 
+ 
 public function filtre($a)
 {
     $a_nettoye = htmlspecialchars($a, ENT_QUOTES, 'UTF-8');
     return  $a_nettoye;
 }
+
 
 public function get_marque()
 {
@@ -40,10 +43,9 @@ public function get_marque()
 
 public function set_marque($marque)
 {
-    $marque = filter_var($marque, FILTER_SANITIZE_EMAIL);
-    return $this->marque=$marque;
+   $marque = $this->filtre($marque);
+    $this->marque = $marque;
 }
-
 
 public function get_immatriculation()
 {
@@ -52,19 +54,19 @@ public function get_immatriculation()
 
 public function set_immatriculation($immatriculation)
 {
-    $immatriculation = filter_var($immatriculation, FILTER_SANITIZE_EMAIL);
-    return $this->immatriculation=$immatriculation;
+    $immatriculation = $this->filtre($immatriculation);
+    $this->immatriculation = $immatriculation;
 }
 
 public function get_modele()
 {
     return $this->modele;
 }
- 
+
 public function set_modele($modele)
 {
-    $modele = filter_var($modele, FILTER_SANITIZE_EMAIL);
-    return $this->modele=$modele;
+   $modele = $this->filtre($modele);
+    $this->modele = $modele;
 }
 
 public function get_kilometrage()
@@ -74,8 +76,8 @@ public function get_kilometrage()
 
 public function set_kilometrage($kilometrage)
 {
-    $kilometrage = filter_var($kilometrage, FILTER_SANITIZE_EMAIL);
-    return $this->kilometrage=$kilometrage;
+   $kilometrage = $this->filtre($kilometrage);
+    $this->kilometrage = $kilometrage;
 }
 
 public function get_annee()
@@ -85,8 +87,8 @@ public function get_annee()
 
 public function set_annee($annee)
 {
-    $annee = filter_var($annee, FILTER_SANITIZE_EMAIL);
-    return $this->annee=$annee;
+   $annee = $this->filtre($annee);
+    $this->annee = $annee;
 }
 
 public function get_id_client()
@@ -96,8 +98,7 @@ public function get_id_client()
 
 public function set_id_client($id_client)
 {
-    $id_client = filter_var($id_client, FILTER_SANITIZE_EMAIL);
-    return $this->id_client=$id_client;
+    $id_client = $this->filtre($id_client);
+    $this->id_client =$id_client;
 }
-
  }
--- Login/verify/verify-voiture.php
+++ Login/verify/verify-voiture.php
@@ -10,8 +10,12 @@ $modele=$_POST['modele'];
 $kilometrage=$_POST['kilometrage'];
 $imma=$_POST['imma1'].'-'.$_POST['imma2'].'-'.$_POST['imma3'];
 $annee = date('Y', strtotime($_POST['annee']));
+$id='r';
 
 
+$voiture = new Voiture($marque,$imma,$modele,$kilometrage,$annee,$id);
+
+echo $voiture->get_modele();
 
 
 }
