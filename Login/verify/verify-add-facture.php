0ca52f952e85437d0fc8ccc92684a0eaa2d6922b
neleph <luc.moutanin@gmail.com>
2024-04-30 05:26:46 +0400

    modification facture

--- Login/verify/verify-add-facture.php
+++ Login/verify/verify-add-facture.php
@@ -25,18 +25,18 @@ if (isset($_POST['ok'])) {
     if (!empty($id_voiture) && !empty($date) && !empty($description1)) {
 
         $langages = array($description1, $description2, $description3, $description4, $description5);
+        $facture = new Facture($id_client, $id_voiture, $date, 0, 0, 0, 0, $repondre['Id_reparation']);
+
+        $requete = $bdd->prepare("INSERT INTO facture (date_facture,Id_client,Id_voiture) VALUES (?,?,?)");
+        $requete->bindParam(1, $facture->get_date());
+        $requete->bindParam(2, $facture->get_id_client());
+        $requete->bindParam(3, $facture->get_id_voiture());
+        $requete->execute();
+        $id_facture = $bdd->lastInsertId();
+
+
 
         foreach ($langages as $langage) {
-
-
-            $facture = new Facture($id_client, $id_voiture, $date, 0, 0, 0, 0, $repondre['Id_reparation']);
-
-            $requete = $bdd->prepare("INSERT INTO facture (date_facture,Id_client,Id_voiture) VALUES (?,?,?)");
-            $requete->bindParam(1, $facture->get_date());
-            $requete->bindParam(2, $facture->get_id_client());
-            $requete->bindParam(3, $facture->get_id_voiture());
-            $requete->execute();
-            $id_facture = $bdd->lastInsertId();
 
             $requete = $bdd->prepare("INSERT INTO facture_reparation (Id_facture,Id_reparation) VALUES (?,?)");
             $requete->bindParam(1,  $id_facture);
